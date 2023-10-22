b9ef91fbfff9d34be292c9ab943993d89142515c
Karan Vaishnav <85957167+karan-vaishnav@users.noreply.github.com>
2023-10-22 23:07:18 +0530

    Adding a process section

---
+++ src/components/OurTeam.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const OurTeam = () => {
+  return (
+    <div className="mx-20">
+      <div className="text-center mt-20 text-4xl font-semibold">
+        Our Team
+      </div>
+        
+    </div>
+  );
+};
+
+export default OurTeam;
